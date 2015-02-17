6387bd61c0de3f448661bbd8398d332e53c77382
Mischa ter Smitten <mischa@tersmitten.nl>
2015-02-17 14:38:04 +0100

    Removal of unused (legacy) code

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 
 setup(name='glibc-locale-tools',
-      version='0.1.1',
+      version='0.2.0',
       author='Mischa ter Smitten',
       author_email='mtersmitten@oefenweb.nl',
       maintainer='Mischa ter Smitten',
