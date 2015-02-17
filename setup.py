b02a21144d9435f3bef190be2964480a1de35888
Mischa ter Smitten <mischa@tersmitten.nl>
2015-02-17 12:45:17 +0100

    Tag initial version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 
 setup(name='glibc-locale-tools',
-      version='0.0.1',
+      version='0.1.0',
       author='Mischa ter Smitten',
       author_email='mtersmitten@oefenweb.nl',
       maintainer='Mischa ter Smitten',
