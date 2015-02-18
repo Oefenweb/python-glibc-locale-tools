104ad5c871da7da36f1269791245036ea90dc4ab
Mischa ter Smitten <mischa@tersmitten.nl>
2015-02-18 17:19:30 +0100

    Added some more tests

--- glibc_locale_tools/test/test_helper_functions.py
+++ glibc_locale_tools/test/test_helper_functions.py
@@ -2,6 +2,7 @@
 
 import unittest
 from glibc_locale_tools.glibc_locale_tools import *
+import re
 
 
 class TestHelperFunctions(unittest.TestCase):
@@ -70,6 +71,63 @@ class TestHelperFunctions(unittest.TestCase):
     expected = iter([3, 2, 1])
 
     self.assertSequenceEqual(list(actual), list(expected))
+
+  def test_between_quotes(self):
+    """
+    Tests BETWEEN_QUOTES_PATTERN.
+    """
+
+    # Multiline, multi value
+    str1 = ('abday   "zo";"ma";"di";/\n'
+            '	"wo";"do";"vr";/\n'
+            '	"za"')
+
+    actual1 = re.findall(BETWEEN_QUOTES_PATTERN, str1)
+    expected1 = ['zo', 'ma', 'di', 'wo', 'do', 'vr', 'za']
+    self.assertSequenceEqual(actual1, expected1)
+
+    # Multiline, multiple single values
+    str2 = ('day     "zondag";/\n'
+            '	"maandag";/\n'
+            '	"dinsdag";/\n'
+            '	"woensdag";/\n'
+            '	"donderdag";/\n'
+            '	"vrijdag";/\n'
+            '	"zaterdag"')
+
+    actual2 = re.findall(BETWEEN_QUOTES_PATTERN, str2)
+    expected2 = ['zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag']
+    self.assertSequenceEqual(actual2, expected2)
+
+    # Single line, single value
+    str3 = 'd_t_fmt "%a %d %b %Y %T %Z"'
+
+    actual3 = re.findall(BETWEEN_QUOTES_PATTERN, str3)
+    expected3 = ['%a %d %b %Y %T %Z']
+    self.assertSequenceEqual(actual3, expected3)
+
+    # Single line, multi (empty) value
+    str4 = 'am_pm   "";""'
+
+    actual4 = re.findall(BETWEEN_QUOTES_PATTERN, str4)
+    expected4 = ['', '']
+    self.assertSequenceEqual(actual4, expected4)
+
+    # Single line, single (empty) value
+    str5 = 't_fmt_ampm ""'
+
+    actual5 = re.findall(BETWEEN_QUOTES_PATTERN, str5)
+    expected5 = ['']
+    self.assertSequenceEqual(actual5, expected5)
+
+    # Multi line, single value
+    str6 = ('date_fmt       "%a %b %e/\n'
+            ' %H:%M:%S /\n'
+            '%Z %Y"')
+
+    actual6 = re.findall(BETWEEN_QUOTES_PATTERN, str6)
+    expected6 = ['%a %b %e/\n %H:%M:%S /\n%Z %Y']
+    self.assertSequenceEqual(actual6, expected6)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestHelperFunctions)
 unittest.TextTestRunner(verbosity=2).run(suite)
